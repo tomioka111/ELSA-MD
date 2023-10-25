1af719108341d0c7526ac19675d64aed95f5ee2e
ELGAZAR_ALWAZER❤ <108340300+Sjshskdhd@users.noreply.github.com>
2023-10-25 16:12:36 +0300

    Update downloader.js

--- commands/downloader.js
+++ commands/downloader.js
@@ -83,99 +83,7 @@ cmd({
     }
 )
 
-//---------------------------------------------------------------------------
 
-cmd({
-            pattern: "apk",
-            desc: "Downloads apks  .",
-            category: "downloader",
-            filename: __filename,
-            use: '<add sticker url.>',
-        },
-
-        async(Void, citel, text) => {
-        if(!text )return citel.reply("*_Give me App Name_*");
-
-	const getRandom = (ext) => { return `${Math.floor(Math.random() * 10000)}${ext}`; };
-	let randomName = getRandom(".apk");
-	const filePath = `./${randomName}`;     // fs.createWriteStream(`./${randomName}`)
-        const {  search , download } = require('aptoide-scraper')
-	let searc = await search(text);          //console.log(searc);
-	let data={};
-	if(searc.length){ data = await download(searc[0].id); }
-	else return citel.send("*_APP not Found, Try Other Name_*");
-	
-	
-	const apkSize = parseInt(data.size);
-	if(apkSize > 100) return citel.send(`❌ File size bigger than 150mb.`);
-       const url = data.dllink;
-	 let  inf  ="*App Name :* " +data.name;
-         inf +="\n*App id        :* " +data.package;
-         inf +="\n*Last Up       :* " +data.lastup;
-         inf +="\n*App Size     :* " +data.size;
-        // inf +="\n*App Link     :* " +data.dllink;
-	inf +="\n\n "+ Config.caption
-         
-
-axios.get(url, { responseType: 'stream' })
-  .then(response => {
-    const writer = fs.createWriteStream(filePath);
-    response.data.pipe(writer);
-
-    return new Promise((resolve, reject) => {
-      writer.on('finish', resolve);
-      writer.on('error', reject);
-    });
-  }).then(() => {
-	
-	let buttonMessage = {
-                        document: fs.readFileSync(filePath),
-                        mimetype: 'application/vnd.android.package-archive',
-                        fileName: data.name+`.apk`,
-                        caption : inf
-                        
-                    }
-                  Void.sendMessage(citel.chat, buttonMessage, { quoted: citel })
-
-    console.log('File downloaded successfully');
-
-  
-    fs.unlink(filePath, (err) => {
-      if (err) { console.error('Error deleting file:', err); } else { console.log('File deleted successfully'); } });
-  }) .catch(error => {
-	fs.unlink(filePath)
-    return citel.send('*_Apk not Found, Sorry_*')//:', error.message);
-  });
-	
-	
-	
-	
-	
-	
-	
-	/*
-  if(!text) return citel.reply(`*_Please Give Me App Name_*`);
-let searc = await search(text);
-//console.log(searc);
-let data = await download(searc[0].id);
-//console.log(data);
-
-     let  inf  ="App Name : " +data.name;
-         inf +="\n*App id        :* " +data.package;
-         inf +="\n*App id        :* " +data.lastup;
-         inf +="\n*App Size     :* " +data.size;
-        // inf +="\n*App Link     :* " +data.dllink;
-         
-                        let buttonMessage = {
-                        document: {url : data.dllink},
-                        mimetype: 'application/vnd.android.package-archive',
-                        fileName: data.name+`.apk`,
-                        caption : inf
-                        
-                    }
-                 Void.sendMessage(citel.chat, buttonMessage, { quoted: citel })
-*/}
-)
     //---------------------------------------------------------------------------
 cmd({
             pattern: "video",
@@ -401,7 +309,7 @@ cmd({
                 let yts = require("secktor-pack")
                 let search = await yts(text);
                 let i = search.all[1] ;
-                let cap = "\t *🎧𝐸𝐿𝐺𝐴𝑍𝐴𝑅 𝑌𝑂𝑈𝑇𝑈𝐵𝐸⃤🎧*   \n\n*֎╎الـعـنـوان📝┇* " + i.title + "\n*֎╎الـرابـط🔗┇* " + i.url +"\n*֎╎الـمـده⏳┇* " + i.timestamp +"\n*֎╎الـمـشـاهـدات📈┇* "+i.views +"\n*֎╎وقـت الـنـشـر🏮┇* " +i.ago +"\n*֎╎الـقـنـاه🎐┇* "+i.author.name+"\n\n\nرد بـ1 للـفـيـديـو \nرد بـ2 للـصـوت" ;
+                let cap = "\t *🎧𝐸𝐿𝐺𝐴𝑍𝐴𝑅 𝑌𝑂𝑈𝑇𝑈𝐵𝐸⃤🎧*   \n\n*֎╎الـعـنـوان📝┇* " + i.title + "\n*֎╎الـرابـط🔗┇* " + i.url +"\n*֎╎الـمـده⏳┇* " + i.timestamp +"\n*֎╎الـمـشـاهـدات📈┇* "+i.views +"\n*֎╎وقـت الـنـشـر🏮┇* " +i.ago +"\n*֎╎الـقـنـاه🎐┇* "+i.author.name+"\n\n\nتكتب 1 لتحميل فيديو \nاكتب 2 لتحميل صوت" ;
                 Void.sendMessage(citel.chat,{image :{url : i.thumbnail}, caption :  cap });
            
            
